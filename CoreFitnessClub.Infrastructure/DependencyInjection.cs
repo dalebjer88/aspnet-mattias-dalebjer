a36d7dc7168f9889ccbabbf83a86b25f8d3e456b
Mattias <dalebjer88@gmail.com>
2026-03-29 13:37:21 +0200

    feat: configure app startup for identity and database

---
+++ CoreFitnessClub.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,49 @@
+﻿using CoreFitnessClub.Infrastructure.Data;
+using CoreFitnessClub.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreFitnessClub.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        string environmentName)
+    {
+        if (environmentName == "Development")
+        {
+            services.AddDbContext<CoreFitnessClubDbContext>(options =>
+                options.UseInMemoryDatabase("CoreFitnessClubDb"));
+        }
+        else
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<CoreFitnessClubDbContext>(options =>
+                options.UseSqlServer(connectionString));
+        }
+
+        services.AddIdentity<AppUser, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 6;
+        })
+            .AddEntityFrameworkStores<CoreFitnessClubDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.ConfigureApplicationCookie(options =>
+        {
+            options.LoginPath = "/Auth/SignIn";
+            options.AccessDeniedPath = "/Auth/AccessDenied";
+        });
+
+        return services;
+    }
+}
--- CoreFitnessClub.Presentation.Mvc/Program.cs
+++ CoreFitnessClub.Presentation.Mvc/Program.cs
@@ -1,8 +1,44 @@
+using CoreFitnessClub.Infrastructure.Data;
+using CoreFitnessClub.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRouting(options =>
 {
     options.LowercaseUrls = true;
+});
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<CoreFitnessClubDbContext>(options =>
+        options.UseInMemoryDatabase("CoreFitnessClubDb"));
+}
+else
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' was not found.");
+
+    builder.Services.AddDbContext<CoreFitnessClubDbContext>(options =>
+        options.UseSqlServer(connectionString));
+}
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+})
+    .AddEntityFrameworkStores<CoreFitnessClubDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Auth/SignIn";
+    options.AccessDeniedPath = "/Auth/AccessDenied";
 });
 
 builder.Services.AddControllersWithViews();
@@ -21,6 +57,7 @@ app.UseRouting();
 
 app.UseStatusCodePagesWithReExecute("/404");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
