823f00d8c29da0469d0032b6576fdb13bf2d5cbb
Mattias <dalebjer88@gmail.com>
2026-05-02 12:12:38 +0200

    Add test project structure

---
+++ CoreFitnessClub.Tests/Integration/TrainingClassIntegrationTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreFitnessClub.Tests.Integration
+{
+    internal class TrainingClassIntegrationTests
+    {
+    }
+}
---
+++ CoreFitnessClub.Tests/Unit/ManageTrainingClassServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreFitnessClub.Tests.Unit
+{
+    internal class ManageTrainingClassServiceTests
+    {
+    }
+}
